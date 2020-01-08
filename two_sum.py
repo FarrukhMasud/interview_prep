2613f0714fc126cc5a9a2b10434a865dc4f3a2df
farrukhmasudali@gmail.com <farrukhmasudali@gmail.com>
2020-01-07 22:45:21 -0800

    optimizin

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,19 @@
 class Solution:
     def twoSum(self, nums, target: int):
-        candidates = dict()
-        for i in range(0, len(nums)):
-            required = target - nums[i]
-            if required in candidates:
-                result = [i, candidates[required]]
-                result.sort()
-                return result
-            candidates[nums[i]] = i
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            sum = nums[left] + nums[right]
+            if sum == target:
+                return [left, right]
+            if sum > target:
+                right = right - 1
+            else:
+                left = left + 1
 
 
 sol = Solution()
-x = sol.twoSum([8, 9, 1, 2, 3, 4, 5, 6], 4)
+y = [8, 9, 1, 2, 3, 4, 5, 6]
+y.sort()
+x = sol.twoSum(y, 4)
 print(x)
