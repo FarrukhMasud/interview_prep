ff4969c752d20330ac1fbf03a1c8af49fdd5bb43
farrukhmasudali@gmail.com <farrukhmasudali@gmail.com>
2020-01-09 23:52:11 -0800

    insertion_sort_list

---
+++ insertion_sort_list.py
@@ -0,0 +1,69 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def insertNode(self, head: ListNode, x: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        dummy.next = head
+        parent = dummy
+        curr = head
+        while curr is not None and curr.val < x.val:
+            parent = curr
+            curr = curr.next
+        t = parent.next
+        parent.next = x
+        if parent.next != t:
+            parent.next.next = t
+        return dummy.next
+
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        head1 = head
+        curr = head.next
+        head1.next = None
+        while curr is not None:
+            t = curr.next
+            curr.next = None
+            head1 = self.insertNode(head1, curr)
+            curr = t
+
+        return head1
+
+
+def createList(vals: List[int]):
+    dummy = ListNode(-1)
+    x = dummy
+    for i in vals:
+        x.next = ListNode(i)
+        x = x.next
+    return dummy.next
+
+
+def printList(head: ListNode):
+    x = head
+    while x is not None:
+        print(x.val)
+        x = x.next
+
+
+def createSortedList(x: List[int]) -> ListNode:
+    head = ListNode(x[0])
+    sol = Solution()
+    for i in range(1, len(x)):
+        y = ListNode(x[i])
+        head = sol.insertNode(head, y)
+    return head
+
+
+lst = createList([0, 4, 2, 1, 3])
+sol = Solution()
+lst = sol.insertionSortList(lst)
+
+printList(lst)
