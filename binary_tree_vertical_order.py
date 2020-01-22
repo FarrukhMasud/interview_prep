30e54ca39f97b01f53cd65af6e4819c3163a643e
farrukhmasudali@gmail.com <farrukhmasudali@gmail.com>
2020-01-21 22:28:21 -0800

    1

--- binary_tree_vertical_order.py
+++ binary_tree_vertical_order.py
@@ -12,32 +12,41 @@ class TreeNode:
 class Solution:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         traversal = dict()
-        minLevel, maxLevel = self._verticalOrder(root, 0, traversal, 0, 0)
+        minLevel, maxLevel = self._verticalOrder(root, 0, traversal, 0, 0, 0)
         result = []
         for i in range(minLevel, maxLevel + 1):
-            result.append(traversal[i])
+            x = traversal[i]
+            x = sorted(x, key=lambda t: t[1])
+            y = [q[0] for q in x]
+            result.append(y)
         return result
 
     def _verticalOrder(
-        self, root: TreeNode, level: int, traversal, minLevel: int, maxLevel: int
+        self,
+        root: TreeNode,
+        level: int,
+        traversal,
+        minLevel: int,
+        maxLevel: int,
+        depth: int,
     ):
         if root is None:
             return minLevel, maxLevel
         if level in traversal:
-            traversal[level].append(root.val)
+            traversal[level].append((root.val, depth))
         else:
-            traversal[level] = [root.val]
+            traversal[level] = [(root.val, depth)]
         temp_min = minLevel
         temp_max = maxLevel
         if root.left is not None:
             temp_min, temp_max = self._verticalOrder(
-                root.left, level - 1, traversal, minLevel - 1, maxLevel
+                root.left, level - 1, traversal, minLevel - 1, maxLevel, depth + 1
             )
         temp_min1 = minLevel
         temp_max1 = maxLevel
         if root.right is not None:
             temp_min1, temp_max1 = self._verticalOrder(
-                root.right, level + 1, traversal, minLevel, maxLevel + 1
+                root.right, level + 1, traversal, minLevel, maxLevel + 1, depth + 1
             )
 
         return min(temp_min, temp_min1, minLevel), max(maxLevel, temp_max1, temp_max)
