908426dfc50253714b59691fa56417ad55fbf6c6
farrukhmasudali@gmail.com <farrukhmasudali@gmail.com>
2020-01-28 12:27:53 -0800

    before and after

---
+++ before_after_puzzle.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class Solution:
+    def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
+        firstWord = dict()
+
+        for phrase in phrases:
+            sp = phrase.split(sep=" ")
+            if len(sp) == 0:
+                continue
+            if sp[0] in firstWord:
+                firstWord[sp[0]].append(phrase)
+            else:
+                firstWord[sp[0]] = [phrase]
+
+        result = []
+        for phrase in phrases:
+            sp = phrase.split(sep=" ")
+            if sp[-1] in firstWord:
+                x = " ".join(sp[0:-1])
+                y = firstWord[sp[-1]]
+                for z in y:
+                    if z == phrase:
+                        continue
+                    result.append((x + " " + z).strip())
+
+        return sorted(result)
+
+
+arr = [
+    "mission statement",
+    "a quick bite to eat",
+    "a chip off the old block",
+    "chocolate bar",
+    "mission impossible",
+    "a man on a mission",
+    "block party",
+    "eat my words",
+    "bar of soap",
+]
+
+# arr = ["a", "b", "a"]
+result = Solution().beforeAndAfterPuzzles(arr)
+print(result)
