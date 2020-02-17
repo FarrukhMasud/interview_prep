12b49fb16e8201ca1d357f5cb5395bed90014fd6
farrukhmasudali@gmail.com <farrukhmasudali@gmail.com>
2020-02-17 11:27:27 -0800

    hard and not passing all leetcode test

---
+++ edit_distance.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        if len(word1) == 0:
+            return len(word2)
+        if len(word2) == 0:
+            return len(word1)
+        if not word1 or not word2:
+            return 0
+
+        dp = [[0] * (len(word1) + 1) for _ in range(0, len(word2) + 1)]
+        for i in range(0, len(word1) + 1):
+            dp[0][i] = i
+        for i in range(0, len(word2) + 1):
+            dp[i][0] = i
+
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[i])):
+                if word1[j - 1] != word2[i - 1]:
+                    dp[i][j] = min(dp[i - 1][j], dp[i - i][j - 1], dp[i][j - 1]) + 1
+                else:
+                    dp[i][j] = dp[i - 1][j - 1]
+
+        return dp[len(word2)][len(word1)]
+
+
+word1 = "sea"
+word2 = "ate"
+result = Solution().minDistance(word1, word2)
+print(result)
