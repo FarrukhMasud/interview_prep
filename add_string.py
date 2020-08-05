7aaccb750d9487f6655f42e06f80d9d6d453ee17
FarrukhMasud <farrukhmasudali@gmail.com>
2020-08-04 22:34:49 -0700

    optimizing the solution
    there is no need to recopy array eveytime

--- add_string.py
+++ add_string.py
@@ -5,22 +5,24 @@ class Solution:
 
         carry = 0
         result = ""
-        while len(num1) > 0 or len(num2) > 0:
-            x = int(num1[-1]) if len(num1) > 0 else 0
-            y = int(num2[-1]) if len(num2) > 0 else 0
+        i = len(num1) - 1
+        j = len(num2) - 1
+        while i >= 0 or j >= 0:
+            x = int(num1[i]) if i >= 0 else 0
+            y = int(num2[j]) if j >= 0 else 0
             sum1 = x + y + carry
             carry = int(sum1 / 10)
             sum1 = int(sum1 % 10)
             result = str(sum1) + result
-            num1 = num1[0:-1]
-            num2 = num2[0:-1]
+            i -= 1
+            j -= 1
 
         if carry > 0:
             result = str(carry) + result
         return result
 
 
-n = "1"
+n = "12345"
 m = "1000"
 result = Solution().addStrings(n, m)
 print(result)
